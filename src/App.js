d298a5c19a8f17dfbd61f4522b27df15f0a5b341
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-07-20 19:26:41 +0530

    using spread operator

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ function App() {
   console.log(count);
  } 
 
+ let obj = {
+  title : "1st Counter",
+  count,
+  place:"hello"
+ }
+
   return (
     <div className='App'>
       <button onClick={addCount}>Add</button>
-      <Counter title="1st Counter" count = {count} />
+      <Counter title="1st Counter" {...obj} />
       <Counter title="2nd Counter" count = {count} />
     </div>
   );
