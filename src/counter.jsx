840b9c4fa6c3483679eb08c61096e8c41cd9ffb5
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2023-07-20 19:17:58 +0530

    also done by destructuring the code simply

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function Counter(props) {
+function Counter(props) { //or here we can take here like funtion Counter({title,count})...
+   
+    //then we dont need this step
+    // without calling props everytime we also can call it like this
+    const {title,count} = props // but here we need to assign to the same name as there or it will get error 
+    //here we are doing destructuring like array-destructuring
+
+
   return (
     <div>
-        <h1>{props.title} {props.count}</h1>
+        <h1>{title} {count}</h1>
         </div>
   )
 }
